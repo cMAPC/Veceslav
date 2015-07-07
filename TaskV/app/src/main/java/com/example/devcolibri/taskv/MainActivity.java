362c75ddca3d65ffee09116ea92c2029be10b75d
cMAPC <s.marcel94@gmail.com>
2015-07-07 10:14:06 +0300

    refactor commit

--- TaskV/app/src/main/java/com/example/devcolibri/taskv/MainActivity.java
+++ TaskV/app/src/main/java/com/example/devcolibri/taskv/MainActivity.java
@@ -29,9 +29,8 @@ import retrofit.client.Response;
 
 public class MainActivity extends Activity {
 
-    EditText editText;
+    private EditText editText;
 
-    //=============================ERROR==========================================
     private boolean haveNetworkConnection() {
         boolean haveConnectedWifi = false;
         boolean haveConnectedMobile = false;
@@ -48,93 +47,76 @@ public class MainActivity extends Activity {
         }
         return haveConnectedWifi || haveConnectedMobile;
     }
-    //=============================ON CREATE========================================
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView textView;
-        final ListView listView;
-        final ArrayList<User> usersList;
-        final List<String> urlList = new ArrayList<String>();
+        final ArrayList<User> usersList = new ArrayList<>();
 
         Button btn = (Button) findViewById(R.id.button);
         editText = (EditText) findViewById(R.id.editText);
-        textView = (TextView) findViewById(R.id.textView);
-        listView = (ListView) findViewById(R.id.listView);
+        final ListView listView = (ListView) findViewById(R.id.listView);
 
-
-        //=====================ON CLICK============================================
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
-                if (haveNetworkConnection()) {
-                    if (!editText.getText().toString().matches("")) {
-
-                        //======================ADAPTER===================
-                        RestAdapter restAdapter = new RestAdapter.Builder()
-                                .setEndpoint("https://api.github.com")
-                                .build();
-                        MethodsAPI service = restAdapter.create(MethodsAPI.class);
-
-
-                        service.listUser(editText.getText().toString(), new Callback<List<User>>() {
-                            @Override
-                            public void success(List<User> users, Response response) {
-
-                                ArrayList<User> usersList = new ArrayList<>();
-                                UserAdapter adapter;
-                                adapter = new UserAdapter(getBaseContext(), R.layout.row, usersList);
-                                for (int i = 0; i < users.size(); i++) {
-                                    User obj = new User();
-                                    obj.setName("Name : " + users.get(i).getName());
-                                    obj.setDescription("Description : " + users.get(i).getDescription());
-                                    //obj.setUrl(users.get(i).getUrl());
-                                    usersList.add(obj);
-                                    urlList.add(i, users.get(i).getHtml_url());
-                                }
-
-                                listView.setAdapter(adapter);
-                            }
-
-                            @Override
-                            public void failure(RetrofitError error) {
-                                Toast.makeText(getApplicationContext(), "Enter a valide name !", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-
-
-                    } else {
-                        Toast.makeText(getApplicationContext(), "Enter a name pleas in the text box !", Toast.LENGTH_SHORT).show();
-                    }
-                } else {
-                    Toast.makeText(getApplicationContext(), "No internet conection, try later !", Toast.LENGTH_SHORT).show();
+                if(everyThingIsValid()){
+                    executeHTTPRequest(editText, usersList, listView);
                 }
-
             }
         });
 
-
-        //=========================ON CLICK LIST=============================================
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                Bundle b = new Bundle();
-                b.putString("URL", urlList.get(position));
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                intent.putExtras(b);
-                startActivity(intent);
-                Toast.makeText(getApplicationContext(), "Item " + position, Toast.LENGTH_SHORT).show();
-
+                displayRepoUrl(position, usersList);
             }
         });
+    }
+
+    private void displayRepoUrl(int position, ArrayList<User> usersList) {
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        intent.putExtra("URL", usersList.get(position).getHtml_url());
+        startActivity(intent);
+        Toast.makeText(getApplicationContext(), "Item " + position, Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean everyThingIsValid() {
+        if (!haveNetworkConnection()) {
+            Toast.makeText(getApplicationContext(), "No internet conection, try later !", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if(editText.getText().toString().matches("")){
+            Toast.makeText(getApplicationContext(), "Enter a name pleas in the text box !", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
+    }
+
+    private void executeHTTPRequest(EditText editText, final ArrayList<User> usersList, final ListView listView) {
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint("https://api.github.com")
+                .build();
+        MethodsAPI service = restAdapter.create(MethodsAPI.class);
 
 
+        service.listUser(editText.getText().toString(), new Callback<List<User>>() {
+            @Override
+            public void success(List<User> users, Response response) {
+                UserAdapter adapter = new UserAdapter(getBaseContext(), users);
+                usersList.addAll(users);
+                listView.setAdapter(adapter);
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Toast.makeText(getApplicationContext(), "Enter a valide name !", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 }
--- TaskV/app/src/main/java/com/example/devcolibri/taskv/SecondActivity.java
+++ TaskV/app/src/main/java/com/example/devcolibri/taskv/SecondActivity.java
@@ -1,36 +1,19 @@
 package com.example.devcolibri.taskv;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.widget.TextView;
 
-public class SecondActivity extends Activity{
-
+public class SecondActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
-        String url;
-        Intent intent = getIntent();
-        Bundle b = intent.getExtras();
-        url = b.getString("URL");
         WebView myWebView = (WebView) findViewById(R.id.webview);
-       // WebSettings webSettings = myWebView.getSettings();
-        //webSettings.setJavaScriptEnabled(true);
+
+        String url = getIntent().getStringExtra("URL");
         myWebView.loadUrl(url);
-
-
-
     }
-
-
-
-
 }
 
